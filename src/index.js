019f878b6d9d03090b43497e68c5e9eb2d60e82e
Justin LI <lijt2007@hotmail.com>
2019-10-11 14:43:41 +1030

    add plugin 'html-webpack-plugin' for auto generate index.html

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
+import printMe from './print';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   //add img
   const tick = new Image();
